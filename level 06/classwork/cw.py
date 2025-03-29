dc63bd63f9b0d9f5d702bcf8dc3c3c95e538da8a
lomiashvili <datolomiashvili545@gmail.com>
2025-03-30 01:00:43 +0400

    second commit

---
+++ level 06/classwork/cw.py
@@ -0,0 +1,18 @@
+num = int(input("Enter number: ")) 
+num1 = int(input("Enter number: ")) 
+print(num - num1)
+
+num2= int(input("Enter number: ")) 
+num3 = int(input("Enter number: ")) 
+print(num2 * num3)
+
+num2= int(input("Enter number: ")) 
+num3 = int(input("Enter number: ")) 
+print(num2 + num3)
+
+num2= int(input("Enter number: ")) 
+num3 = int(input("Enter number: ")) 
+print(num2 / num3)
+
+
+
