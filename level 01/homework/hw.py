fab298d6c7bb3d4d1372fcac715a07d71dd3943f
lomiashvili <datolomiashvili545@gmail.com>
2025-04-07 23:31:55 +0400

    second commit

--- level 01/homework/hw.py
+++ level 01/homework/hw.py
@@ -1,2 +1,8 @@
+name = "dato"
+lastname = "lomiashvili"
+age = 15
+height = 185
+print(f"my name is {name} my last name is {lastname} im {age} years old and im {height} cm tall") 
 
-# აქ არ ვქონია არაფერი
+
+
